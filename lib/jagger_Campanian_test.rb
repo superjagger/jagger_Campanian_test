ba0ee2a29f82e9f6854cf7e2a76d69d1e67d0241
superjagger <31763819+superjagger@users.noreply.github.com>
2024-03-02 21:22:01 +0800

    Add files via upload

---
+++ lib/jagger_Campanian_test.rb
@@ -0,0 +1,6 @@
+require "jagger_Campanian_test/version"
+
+module JaggerCampanianTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jagger_Campanian_test/version.rb
@@ -0,0 +1,3 @@
+module JaggerCampanianTest
+  VERSION = "0.1.0"
+end
